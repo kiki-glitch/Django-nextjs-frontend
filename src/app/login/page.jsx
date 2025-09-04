c3339a6863882ba2458aef9bd0a9ce548ada14a4
unknown <kikialex37@gmail.com>
2025-09-04 14:14:48 +0300

    Login Page with shadcn ui

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,45 +1,14 @@
 // -> url -> /login
 "use client";
 
-import { useAuth } from "@/components/authProvider";
+import { LoginForm } from "@/components/login-form"
 
-const LOGIN_URL = "/api/login/"
-
-export default function Page(){
-    const auth = useAuth()
-
-    async function handleSubmit(event){
-        event.preventDefault()
-        console.log(event, event.target)
-        const formData = new FormData(event.target)
-        const objectFromForm = Object.fromEntries(formData)
-        const jsonData = JSON.stringify(objectFromForm)
-        const requestOptions = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body:jsonData
-        }
-        const response = await fetch(LOGIN_URL, requestOptions)
-        const rData = await response.json()
-        console.log(rData)
-        if (response.ok){
-            console.log("logged in")
-            auth.login()
-        }
-    }
-
-    return <div className="h-[95vh]">
-        <div className="max-w-md mx-auto py-5">
-            <h1>Login Here</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="text" required name="username" placeholder="Your Username"/>
-                <input type="password" required name="password" placeholder="Your Password"/>
-                <button type="submit">Login</button>
-            </form>
-        </div>
+export default function Page() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
     </div>
-    
-    
+  )
 }
