6d9a2607176e6fcae24b95e6da1e8e33de38e59a
unknown <kikialex37@gmail.com>
2025-09-17 18:46:59 +0300

    Rendering DjangoForm Validation Errors in Nextjs

--- src/app/waitlists/forms.jsx
+++ src/app/waitlists/forms.jsx
@@ -1,53 +1,61 @@
 import { cn } from "@/lib/utils"
-import { useAuth } from "@/components/authProvider"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useState } from "react"
 
-const WAITLIST_URL = "/api/waitlists/";
+const WAITLIST_API_URL = "/api/waitlists/";
 
 export function WaitListForm({className,...props}) {
 
     const[message, setMessage] = useState("")
+    const[errors, setErrors] = useState("")
     const[error, setError] = useState("")
 
     async function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const objectFromForm = Object.fromEntries(formData);
-    const jsonData = JSON.stringify(objectFromForm);
+      event.preventDefault();
+      setMessage("")
+      setErrors("")
+      setError("")
+      const formData = new FormData(event.target);
+      const objectFromForm = Object.fromEntries(formData);
+      const jsonData = JSON.stringify(objectFromForm);
 
-    try {
-      const response = await fetch(WAITLIST_URL, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: jsonData,
-      });
+      try {
+        const requestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: jsonData,
+        };
 
-    //   const rData = await response.json();
-
-      if (response.ok) {
-        setMessage("Thank you for joining")
-      } else {
-        setError("There was an error with your request. Please try again");
+        const response = await fetch(WAITLIST_API_URL, requestOptions);
+        const data = await response.json()
+        console.log("Response data:", data)
+        if (response.status === 201 || response.status === 200) {
+          setMessage("Thank you for joining")
+        } else {
+          setErrors(data)
+          if (!data.email){
+            setError("There was an error with your request. Please try again");
+          }
+        }
+      } catch (err) {
+        console.error("Error:", err);
       }
-    } catch (err) {
-      console.error("Error:", err);
-    }
   }
 
   return (
     <div className={cn("gap-1", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="p-0">
-          <form onSubmit={handleSubmit} className="p-6 md:p-8">
-            <div>{message && message}</div>
-            <div>{error && error}</div>
+          <form onSubmit={handleSubmit} className="p-6 md:p-8 space-y-4">
+            {message && <div className='rounded-md bg-accent p-3 font-semibold text-sm'>{message}</div>}
+            {error &&<div className='rounded-md text-white bg-destructive p-3 font-semibold text-sm'>{error}</div>}
             <div className="flex flex-col gap-6">
               <div className="grid gap-3">
                 <Label htmlFor="email">Email</Label>
+                <div className={errors?.email ? "rounded-lg p-3 border border-destructive" : ''}>
                 <Input
                   id="email"
                   name="email"
@@ -55,6 +63,15 @@ export function WaitListForm({className,...props}) {
                   placeholder="Enter your email"
                   required
                 />
+                {errors && errors?.email && errors?.email.length > 0 && <div className='p-1 text-sm bg-destructive text-center text-white'>
+                  {errors?.email.map((err, idx) => {
+                    return !err.message ? null : <p key={`err-${idx}`}>
+                      {err.message}
+                    </p>
+                  })}
+                  </div>
+                }
+              </div>
               </div>
               <Button type="submit" className="w-full">
                 Join Waitlist
